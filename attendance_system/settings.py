662bf57bb3c7f534c1992b97a4bbd559a107dc1d
kumarborkar22 <borkarkumar22@gmail.com>
2025-02-20 18:01:15 +0530

    image update

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'attendance'
+    'attendance',
+    'rest_framework',
 ]
 
 import os
@@ -123,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Default primary key field type
